7840212b7f7ae9e6171da7ec241eb6969aa43cd2
felipecarrillo100 <felipecarrillo100@gmail.com>
2026-03-15 19:09:35 +0100

    Fixes bug in the baker tutorial sample

--- tutorial/solutions/4.1-baker.js
+++ tutorial/solutions/4.1-baker.js
@@ -15,14 +15,17 @@ const API_ENDPOINT = "__API_ENDPOINT__";
 exports.handler = async (event) => {
     const request = event.Records[0].cf.request;
 
+    console.log(`[Production] API_ENDPOINT to: ${API_ENDPOINT}`);
+
     /**
      * 🛠️ Variable Resolution:
-     * We check if the placeholder was successfully swapped.
-     * If not (e.g., during local dev), we fallback to a local URL.
+     * We check if the placeholder was successfully swapped with a regex to verify __VAR_NAME__.
+     * If not (e.g., during local dev), we fall back to a local URL.
      */
-    const api = (typeof API_ENDPOINT !== 'undefined' && API_ENDPOINT !== "__API_ENDPOINT__")
+    const isPlaceholder = new RegExp("^" + "__" + ".*" + "__" + "$").test(API_ENDPOINT);
+    const api = !isPlaceholder
         ? API_ENDPOINT
-        : 'http://localhost:8080';
+        : 'http://localhost:8080'; // Local development fallback
 
     // Logs appear in the emulator terminal or CloudWatch Logs in production.
     console.log(`[Production] Connecting to: ${api}`);
